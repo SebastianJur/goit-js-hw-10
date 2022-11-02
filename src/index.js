a20936fae84c1959f314f8c3ac8beb40e8c49fbb
Sebastian Jurewicz <senkartan@gmail.com>
2022-11-02 21:10:22 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+
+const countryInfo = document.querySelector('.country-info');
+const searchInput = document.querySelector('#search-box');
+
+searchInput.addEventListener('input', debounce(onIput, DEBOUNCE_DELAY));
+
+function onIput(event) {
+  let countryName = event.target.value.trim();
+  console.log(countryName);
+  if (searchInput.value.length === 0) {
+    countryInfo.innerHTML = '';
+    return Notify.info('Type the name of country');
+  }
+  if (!countryName) {
+    return;
+  }
+  return fetchCountries(countryName);
+}
